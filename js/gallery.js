aab877489e14b5abceedbc81606d33b3a3141319
zkduser <zkduserteste@gmail.com>
2025-09-18 23:46:15 -0300

    2025 Definitivo

--- js/gallery.js
+++ js/gallery.js
@@ -1,9 +1,13 @@
 const galleryData = [
     { type: 'photos', url: 'https://live.staticflickr.com/65535/54037393376_ea6fa45db6_b.jpg' },
+    { type: 'photos', url: 'https://live.staticflickr.com/65535/54037654483_0970bac0a3_z.jpg' },
+    { type: 'photos', url: 'https://live.staticflickr.com/65535/54037849355_05d6a81fd4_z.jpg' },
+    {type: 'photos', url: 'https://live.staticflickr.com/65535/54037848390_c99a88e574_b.jpg'},
+    {type: 'photos', url: 'https://live.staticflickr.com/65535/54037718959_02c43d26c8_z.jpg'},
     { type: 'photos', url: 'https://live.staticflickr.com/65535/54037394411_b2151d8d46_b.jpg' },
     { type: 'photos', url: 'https://live.staticflickr.com/65535/54037847815_2536b538e6_z.jpg' },
     {type: 'photos', url:  'https://live.staticflickr.com/65535/54037720494_6fb58b1a97_z.jpg' },
-    { type: 'videos', url: '.mp4' }
+    { type: 'videos', url: 'SescMUN CJ 2024.mp4' }
 ];
 
 export function initializeGallery() {
